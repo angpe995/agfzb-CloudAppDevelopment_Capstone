b103e173c3e5b784a8f9740baa0060a899787227
angpe995 <angpe995@gmail.com>
2023-01-11 18:59:34 -0500

    commit

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -2,13 +2,64 @@ import requests
 import json
 # import related models here
 from requests.auth import HTTPBasicAuth
-
+from .models import CarDealer
 
 # Create a `get_request` to make HTTP GET requests
 # e.g., response = requests.get(url, params=params, headers={'Content-Type': 'application/json'},
 #                                     auth=HTTPBasicAuth('apikey', api_key))
 
+def get_request(url, **kwargs):
+    print(kwargs)
+    print("GET from {} ".format(url))
+    try:
+        # Call get method of requests library with URL and parameters
+        response = requests.get(url, headers={'Content-Type': 'application/json'},
+                                    params=kwargs)
+    except:
+        # If any error occurs
+        print("Network exception occurred")
+    status_code = response.status_code
+    print("With status {} ".format(status_code))
+    json_data = json.loads(response.text)
+    return json_data
 
+def get_dealers_from_cf(url, **kwargs):
+    results = []
+    json_result = get_request(url)
+    # Retrieve the dealer data from the response
+    print("line 55 RA", json_result)
+    dealers = json_result
+    # For each dealer in the response
+    for dealer in dealers:
+        # Get its data in `doc` object
+        dealer_doc = dealer["doc"]
+        # Create a CarDealer object with values in `doc` object
+        dealer_obj = CarDealer(address=dealer_doc["address"], city=dealer_doc["city"], full_name=dealer_doc["full_name"],
+                               id=dealer_doc["id"], lat=dealer_doc["lat"], long=dealer_doc["long"],
+                               short_name=dealer_doc["short_name"],
+                               st=dealer_doc["st"], zip=dealer_doc["zip"])
+        results.append(dealer_obj)
+
+    return results
+
+def get_dealer_by_id(url, dealerId):
+    results = []
+    json_result = get_request(url)
+    # Retrieve the dealer data from the response
+    print("line 55 RA", json_result)
+    dealers = json_result
+    # For each dealer in the response
+    for dealer in dealers:
+        # Get its data in `doc` object
+        dealer_doc = dealer["doc"]
+        # Create a CarDealer object with values in `doc` object
+        dealer_obj = CarDealer(address=dealer_doc["address"], city=dealer_doc["city"], full_name=dealer_doc["full_name"],
+                               id=dealer_doc["id"], lat=dealer_doc["lat"], long=dealer_doc["long"],
+                               short_name=dealer_doc["short_name"],
+                               st=dealer_doc["st"], zip=dealer_doc["zip"])
+        results.append(dealer_obj)
+
+    return results
 # Create a `post_request` to make HTTP POST requests
 # e.g., response = requests.post(url, params=kwargs, json=payload)
 
@@ -29,6 +80,7 @@ from requests.auth import HTTPBasicAuth
 # def analyze_review_sentiments(text):
 # - Call get_request() with specified arguments
 # - Get the returned sentiment label such as Positive or Negative
+
 
 
 
