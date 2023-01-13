b1b0afacb6aae37b54990c6ac3afe20c38ac06ae
angpe995 <angpe995@gmail.com>
2023-01-13 12:25:00 -0500

    ff

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,6 +1,10 @@
 import requests
 import json
 # import related models here
+from ibm_watson import NaturalLanguageUnderstandingV1
+from ibm_watson.natural_language_understanding_v1 \
+    import Features, SemanticRolesOptions,EntitiesOptions
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from requests.auth import HTTPBasicAuth
 from .models import CarDealer,DealerReview
 
@@ -10,13 +14,19 @@ from .models import CarDealer,DealerReview
 
 def get_request(url, **kwargs):
     print(kwargs)
+    api_key=kwargs.get("api_key")
     print("GET from {} ".format(url))
     try:
-        # Call get method of requests library with URL and parameters
-        response = requests.get(url, headers={'Content-Type': 'application/json'},
-                                    params=kwargs)
+        if api_key:
+            params = dict()
+            params["text"] = kwargs["text"]
+            params["version"] = kwargs["version"]
+            params["features"] = kwargs["features"]
+            params["return_analyzed_text"] = kwargs["return_analyzed_text"]
+            response = request.get(requests.get(url, params=params, headers={'Content-Type': 'application/json'},auth=HTTPBasicAuth('apikey', api_key)))
+        else:
+            response = requests.get(url, headers={'Content-Type': 'application/json'}, params=kwargs)
     except:
-        # If any error occurs
         print("Network exception occurred")
     status_code = response.status_code
     print("With status {} ".format(status_code))
@@ -62,11 +72,9 @@ def get_dealer_reviews_from_cf(url, id):
             car_model = review["car_model"]
             car_year = review["car_year"]
             purchase_date = review["purchase_date"]
-
-                # Creating a review object
-            review_obj = DealerReview(id=id, name=name, dealership=dealership, review=review, sentiment=None, purchase=purchase, purchase_date=purchase_date, car_make=car_make, car_model=car_model, car_year=car_year)
+            review_obj = DealerReview(id=id, name=name, dealership=dealership, review=review_content, sentiment=None, purchase=purchase, purchase_date=purchase_date, car_make=car_make, car_model=car_model, car_year=car_year)
             results.append(review_obj)
-        
+            review_obj.sentiment = analyze_review_sentiments(review_obj.review)
     return results
 
 def get_dealer_by_id(url, dealerId, **kwargs):
@@ -100,7 +108,16 @@ def get_dealer_by_id_from_cf(url, id):
 # Create a `post_request` to make HTTP POST requests
 # e.g., response = requests.post(url, params=kwargs, json=payload)
 
-
+def analyze_review_sentiments(dealerreview):
+    print(dealerreview)
+    print(dealerreview)
+    print(dealerreview)
+    api_key = IAMAuthenticator('7hONHM3lp4AbHSvUJ0Ii41uz0oDy01nciohO6U8Xmtk7')
+    natural_language_understanding = NaturalLanguageUnderstandingV1(version='2022-04-07', authenticator=api_key)
+    natural_language_understanding.set_service_url('https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/instances/fc516d31-284d-4459-86d1-9804557e72bd')
+    response = natural_language_understanding.analyze(text=dealerreview,features=Features(entities=EntitiesOptions(sentiment=True,limit=1))).get_result()
+    print(response["entities"]) 
+    return response["entities"]
 # Create a get_dealers_from_cf method to get dealers from a cloud function
 # ef get_dealers_from_cf(url, **kwargs):
 # - Call get_request() with specified arguments
@@ -117,7 +134,3 @@ def get_dealer_by_id_from_cf(url, id):
 # def analyze_review_sentiments(text):
 # - Call get_request() with specified arguments
 # - Get the returned sentiment label such as Positive or Negative
-
-
-
-
